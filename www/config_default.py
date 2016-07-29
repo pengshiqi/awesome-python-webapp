6262ace1c82d8bca4d008bed4687fa79b413273f
彭诗奇 <patrick_psq@pengshiqideMacBook-Pro.local>
2016-07-29 14:51:03 +0800

    day-07

---
+++ www/config_default.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+__author__ = 'patrick_psq'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '123456',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+__author__ = 'patrick_psq'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
