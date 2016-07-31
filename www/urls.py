59fd07a60796fdb05bb7de99a78d9052cea0748a
pengshiqi <617225573@qq.com>
2016-07-31 23:33:01 +0800

    day-08 commit

--- www/urls.py
+++ www/urls.py
@@ -8,8 +8,16 @@ from transwarp.web import get, view
 
 from models import User, Blog, Comment
 
-@view('test_users.html')
+# @view('test_users.html')
+# @get('/')
+# def test_users():
+#     users = User.find_all()
+#     return dict(users=users)
+
+@view('blogs.html')
 @get('/')
-def test_users():
-    users = User.find_all()
-    return dict(users=users)
+def index():
+    blogs = Blog.find_all()
+    # 查找登录用户
+    user = User.find_first('where email=?', 'patrick_psq@outlook.com')
+    return dict(blogs=blogs, user=user)
--- www/wsgiapp.py
+++ www/wsgiapp.py
@@ -4,11 +4,26 @@ __author__ = 'patrick_psq'
 
 import logging; logging.basicConfig(level=logging.INFO)
 import os
+from datetime import datetime
 
 from transwarp import db
 from transwarp.web import WSGIApplication, Jinja2TemplateEngine
 
 from config import configs
+
+# 定义datetime_filter， 输入是t，输出是unicode字符串
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 # init the Database
 db.create_engine(**configs.db)
@@ -18,6 +33,8 @@ wsgi = WSGIApplication(os.path.dirname(os.path.abspath(__file__)))
 
 # init jinja2 template engine
 template_engine = Jinja2TemplateEngine(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+template_engine.add_filter('datetime', datetime_filter)
+
 wsgi.template_engine = template_engine
 
 # 加载带有@get/@post的URL处理函数
@@ -25,4 +42,6 @@ import urls
 wsgi.add_module(urls)
 
 if __name__ == '__main__':
-    wsgi.run(9000)
+    wsgi.run(9000, host='0.0.0.0')
+else:
+    application = wsgi.get_wsgi_application()
